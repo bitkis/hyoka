ed15a9f39234d6060a0cde226698c8f98d42e8f7
Hanif Bin Ariffin <hanif.ariffin.4326@gmail.com>
2020-08-09 16:04:57 -0400

    This should be enough for lisp-calculator.

--- examples/lisp_calculator.rs
+++ examples/lisp_calculator.rs
@@ -1,7 +1,5 @@
 extern crate hyoka;
 
-use std::convert::TryFrom;
-
 struct Env {
     global: std::collections::HashMap<String, Expression>
 }
@@ -19,34 +17,29 @@ impl Env {}
 #[derive(Debug, Clone)]
 enum Expression {
     Float(f32),
-    Integer(u32),
     Symbol(String),
     List(Vec<Expression>),
 }
 
-impl TryFrom<&str> for Expression {
-    type Error = String;
-
-    fn try_from(value: &str) -> Result<Self, Self::Error> {
-        if let Ok(v) = value.parse::<u32>() {
-            Ok(Expression::Integer(v))
-        } else if let Ok(v) = value.parse::<f32>() {
-            Ok(Expression::Float(v))
+impl From<&str> for Expression {
+    fn from(value: &str) -> Self {
+        if let Ok(v) = value.parse::<f32>() {
+            Expression::Float(v)
         } else {
-            Ok(Expression::Symbol(String::from(value)))
+            Expression::Symbol(String::from(value))
         }
     }
 }
 
 impl Expression {
     pub fn parse(tokens: &mut Vec<String>) -> Result<Expression, String> {
-        println!("parsing:{:?}", tokens);
         if tokens.len() == 0 {
             Err(format!("unexpected EOF while parsing the tokens."))
         } else {
             let token = tokens.remove(0);
             match token.as_str() {
                 "(" => {
+                    // TODO: Can we avoid ITM here?
                     let mut L = Vec::<Expression>::new();
                     while tokens[0] != ")" {
                         match Expression::parse(tokens) {
@@ -58,7 +51,87 @@ impl Expression {
                     Ok(Expression::List(L))
                 }
                 ")" => Err(format!("unexpected ')' found while parsing tokens")),
-                _ => Expression::try_from(token.as_str())
+                _ => Ok(Expression::from(token.as_str()))
+            }
+        }
+    }
+
+    pub fn evaluate(&mut self) -> Option<Expression> {
+        match self.clone() {
+            Expression::Float(_) => { Some(self.clone()) }
+            Expression::Symbol(_) => { Some(self.clone()) }
+            Expression::List(mut list) => {
+                let car = list.get(0).expect("Attempting to evaluate an empty Expression::List.");
+                match car.clone() {
+                    Expression::Symbol(mut procedure_name) => {
+                        match procedure_name.as_str() {
+                            "+" => {
+                                let result = list.iter_mut().skip(1).fold(0.0f32, |acc, mut x| {
+                                    match x.evaluate() {
+                                        Some(x) => {
+                                            match x {
+                                                Expression::Float(x) => acc + x,
+                                                _ => panic!("All evaluation should yield Expression::Float")
+                                            }
+                                        }
+                                        _ => panic!("Evaluation failed.")
+                                    }
+                                });
+                                Some(Expression::Float(result))
+                            }
+                            "-" => {
+                                let result = list.iter_mut().skip(1).fold(0.0f32, |acc, mut x| {
+                                    match x.evaluate() {
+                                        Some(x) => {
+                                            match x {
+                                                Expression::Float(x) => acc - x,
+                                                _ => panic!("All evaluation should yield Expression::Float")
+                                            }
+                                        }
+                                        _ => panic!("Evaluation failed.")
+                                    }
+                                });
+                                Some(Expression::Float(result))
+                            }
+                            "*" => {
+                                let result = list.iter_mut().skip(1).fold(1.0f32, |acc, mut x| {
+                                    match x.evaluate() {
+                                        Some(x) => {
+                                            match x {
+                                                Expression::Float(x) => acc * x,
+                                                _ => panic!("All evaluation should yield Expression::Float")
+                                            }
+                                        }
+                                        _ => panic!("Evaluation failed.")
+                                    }
+                                });
+                                Some(Expression::Float(result))
+                            }
+                            "/" => {
+                                let result = list.iter_mut().skip(1).fold(1.0f32, |acc, mut x| {
+                                    match x.evaluate() {
+                                        Some(x) => {
+                                            match x {
+                                                Expression::Float(x) => acc / x,
+                                                _ => panic!("All evaluation should yield Expression::Float")
+                                            }
+                                        }
+                                        _ => panic!("Evaluation failed.")
+                                    }
+                                });
+                                Some(Expression::Float(result))
+                            }
+                            _ => {
+                                println!("unknown procedure '{}' invoked.", procedure_name);
+                                None
+                            }
+                        }
+                    }
+                    _ => {
+                        println!("expected Expression::Symbol to represent procedure's name but found something else...");
+                        None
+                    }
+                }
             }
         }
     }
@@ -66,11 +139,11 @@ impl Expression {
 
 
 pub fn main() {
-    let mut repl = hyoka::Repl::new("lisp-summation>", Env::default(), |x: &mut Env, y: String| {
+    let mut repl = hyoka::Repl::new("lisp-calculator>", Env::default(), |x: &mut Env, y: String| {
         let mut y = y.replace('(', " ( ").replace(')', " ) ").split_ascii_whitespace().
             map(String::from).collect::<Vec<String>>();
         match Expression::parse(&mut y) {
-            Ok(result) => format!("result:{:?}", result),
+            Ok(mut result) => format!("result:{:?}", result.evaluate()),
             Err(e) => panic!(e)
         }
     });
