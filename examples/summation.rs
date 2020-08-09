0206b6ddde481de4caac0a53a13caf8c25720f97
Hanif Bin Ariffin <hanif.ariffin.4326@gmail.com>
2020-08-08 23:48:36 -0400

    Fixed example

--- examples/summation.rs
+++ examples/summation.rs
@@ -2,6 +2,12 @@ extern crate hyoka;
 
 pub fn main() {
     let mut repl = hyoka::Repl::new("repl>", 0, |x: &mut i32, y: String| {
+        let y = {
+            let mut y = y;
+            y.pop();
+            y
+        };
+
         let y = y
             .parse::<i32>()
             .expect(&format!("Cannot convert '{}' into an integer", y));
